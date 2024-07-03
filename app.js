2b361bb13186ce426aa456358749f42adfbc589f
mallquidev <159455849+mallquidev@users.noreply.github.com>
2024-07-03 15:40:42 -0500

    fist commit

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('rick and mory')
+
+async function apimorty(){
+    let api = 'https://rickandmortyapi.com/api/character'
+    try {
+        const response = await fetch(api)
+        if(!response.ok) {
+            throw new Error(`Response status: ${response.status}`)
+        }
+        const json = await response.json()
+        console.log(json)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+apimorty()
